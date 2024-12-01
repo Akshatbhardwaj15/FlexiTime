f9a064829995d98c00a3d7b100417f3653b7fd91
Akshatbhardwaj15 <akshatbhardwaj680@gmail.com>
2024-12-01 23:07:16 +0530

    Beautiful Login Page has been created. Yayyy!!!

--- app/page.js
+++ app/page.js
@@ -2,100 +2,109 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col h-screen w-screen overflow-hidden">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <nav className="bg-white">
+        <div className="py-4 px-6">
+          <img src="/images/logo.png" alt="Logo" className="w-32 h-auto" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </nav>
+
+      <div className="flex flex-grow w-full">
+        <div className="w-1/2 flex flex-col items-center justify-start flex-grow mt-16">
+
+          <div className="w-[844px] h-[475px]">
+            <img
+              src="/images/workout 1.png"
+              alt="Workout"
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
+        <div className="relative w-1/2 flex justify-center items-center bg-white">
+          <div className="max-w-sm w-full p-8">
+            <h2 className="text-3xl font-bold text-left mb-1">Sign In</h2>
+            <p className="font-inter text-[14px] font-extralight text-left leading-6 text-black/30" >Please login to continue to your account.</p>
+            <div className="mt-5">
+              <form action="">
+                <div className="relative mt-6">
+                  <input
+                    type="email"
+                    className="peer w-full h-14 px-6 placeholder-transparent rounded-lg focus:outline-none focus:ring-0 focus:border-blue-600 border border-gray-300 hover:border-blue-600 transition duration-200"
+                    placeholder="name"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="font-inter font-thin absolute left-3 top-4 ml-1 -translate-y-3 bg-white px-1 text-gray-400 text-sm transition-all duration-200 ease-linear peer-placeholder-shown:translate-y-0
+                    peer-placeholder-shown:text-gray-400 peer-focus:top-1 
+                     peer-focus:-translate-y-3 
+                    peer-focus:text-blue-600 peer-focus:text-xs"
+                  >
+                    Email
+                  </label>
+                </div>
+                <div className="relative mt-6">
+                  <input
+                    type="email"
+                    className="peer w-full h-14 px-6 placeholder-transparent rounded-lg focus:outline-none focus:ring-0 focus:border-blue-600 border border-gray-300 hover:border-blue-600 transition duration-200"
+                    placeholder="name"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="font-inter font-thin absolute left-3 top-4 ml-1 -translate-y-3 bg-white px-1 text-gray-400 text-sm transition-all duration-200 ease-linear peer-placeholder-shown:translate-y-0
+                    peer-placeholder-shown:text-gray-400 peer-focus:top-1 
+                     peer-focus:-translate-y-3 
+                    peer-focus:text-blue-600 peer-focus:text-xs"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2 mt-4">
+                  <input
+                    type="checkbox"
+                    id="keep-logged-in"
+                    className="w-4 h-4 border-8 border-black focus:ring-blue-600 focus:ring-2 rounded"
+                  />
+                  <label
+                    htmlFor="keep-logged-in"
+                    className="text-black text-sm font-medium font-inter"
+                  >
+                    Keep me logged in
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 transition duration-200 mt-4"
+                >
+                  Sign in
+                </button>
+
+                <div className="flex items-center mt-4 w-full">
+                  <hr className="w-full border-gray-300"/>
+                  <span className="px-4 text-gray-500 text-md">or</span>
+                  <hr className="w-full border-gray-300"/>
+                </div>
+                
+                <button className="flex items-center justify-center w-full max-w-sm py-4 border border-gray-300 rounded-lg shadow-sm hover:shadow-sm transition duration-200 mt-4">
+                  <span className="text-gray-800 text-sm font-semibold">Sign in with Google</span>
+                  <img 
+                  src="/images/Group.png"
+                  alt="Google Logo"
+                  className="w-5 h-5 ml-2"
+                  />
+                </button>
+
+                  <div className="flex items-center justify-center mt-6 text-sm text-gray-600 gap-1">
+                    Need a account?{" "}
+                    <a href="#" className="text-blue-600 hover:underline">
+                      Create one
+                    </a>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
